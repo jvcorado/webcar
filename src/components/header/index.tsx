1850c50a4e650e8ef098aa8931f9c02f3265a977
jvcorado <jhonisilva545@gmail.com>
2024-04-09 16:12:37 -0300

    header profile auth

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -20,27 +20,29 @@ export default function Header() {
         <img src={Logo} alt="Web Carros" />
       </Link>
 
-      <div
-        className="relative border-2 p-3 rounded-full cursor-pointer "
-        onClick={() => setMenuOpen(!menuOpen)}
-      >
-        <FiUser size={24} color="#000" />
+      {!loadingAuth && signed && (
+        <div
+          className="relative border-2 p-3 rounded-full cursor-pointer "
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <FiUser size={24} color="#000" />
 
-        {menuOpen && (
-          <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg">
-            <button className="block w-full text-left px-4 py-2 text-sm text-black hover:opacity-80">
-              {user?.name}
-            </button>
-            <button
-              className="block w-full text-left px-4 py-2 text-sm text-black "
-              onClick={() => handleLogout()}
-            >
-              <FiLogOut className="inline-block mr-2" color="#E11138" />
-              Sair da conta
-            </button>
-          </div>
-        )}
-      </div>
+          {menuOpen && (
+            <div className="absolute right-0 mt-2 w-48 bg-white border rounded-lg shadow-lg">
+              <button className="block w-full text-left px-4 py-2 text-sm text-black hover:opacity-80">
+                {user?.name}
+              </button>
+              <button
+                className="block w-full text-left px-4 py-2 text-sm text-black "
+                onClick={() => handleLogout()}
+              >
+                <FiLogOut className="inline-block mr-2" color="#E11138" />
+                Sair da conta
+              </button>
+            </div>
+          )}
+        </div>
+      )}
 
       {!loadingAuth && !signed && (
         <Link to={"/login"} className="border-2 p-3 rounded-full ">
