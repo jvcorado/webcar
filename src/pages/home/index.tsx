dcf2569ebfa5ca208b50b46d0cc518e80f5d509a
jvcorado <jhonisilva545@gmail.com>
2024-04-27 15:48:28 -0300

    teste

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -259,7 +259,7 @@ export default function Home() {
               return (
                 <div
                   key={item.id}
-                  className="bg-white sm:max-w-[500px] flex flex-col rounded-lg shadow-xl border-2"
+                  className="bg-white sm:max-w-[300px] flex flex-col rounded-lg shadow-xl border-2"
                 >
                   <Swiper
                     slidesPerView={sliderPerview}
